50bfe9c502a0133fec65ec361b9a64a4ec65b831
ALDAIR301098 <pv18021157@vallarta.tecmm.edu.mx>
2021-09-21 21:30:16 -0500

    Se implemento el SplashScreen de la Api Core de Android

--- app/src/main/java/com/petscare/org/activitys/ActivityWelcome.kt
+++ app/src/main/java/com/petscare/org/activitys/ActivityWelcome.kt
@@ -3,7 +3,9 @@ package com.petscare.org.activitys
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.petscare.org.R
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.petscare.org.databinding.ActWelcomeBinding
 
 class ActivityWelcome : AppCompatActivity() {
@@ -11,17 +13,24 @@ class ActivityWelcome : AppCompatActivity() {
     private lateinit var binding : ActWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //Splashscreen config
-        Thread.sleep(1500)
-        setTheme(R.style.THEME_GLOBAL_APP)
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        eventos()
+        retardarSplashScreen()
+        EventosUI()
     }
 
-    private fun eventos(){
-        binding.btnIngresar.setOnClickListener({mostrarActivity(Intent(this, ActivityAuthentication::class.java))})
+    private fun retardarSplashScreen() {
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener {
+            Thread.sleep(1500)
+            true
+        }
+    }
+
+    private fun EventosUI(){
+        binding.btnIngresar.setOnClickListener {mostrarActivity(Intent(this, ActivityAuthentication::class.java))}
     }
 
     private fun mostrarActivity(intent: Intent){
